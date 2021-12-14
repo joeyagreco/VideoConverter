85256e3957ff3356606dec876a8185eb5668f996
jg <57115019+joeyagreco@users.noreply.github.com>
2021-12-13 22:27:13 -0600

    updated doc url

--- VideoConverter.py
+++ VideoConverter.py
@@ -6,7 +6,8 @@ class VideoConverter:
     def __init__(self, oldVideoPath: str, newVideoPath: str, **kwargs):
         self.__oldVideoPath = oldVideoPath
         self.__newVideoPath = newVideoPath
-        # log levels for ffmpeg listed here: https://superuser.com/questions/326629/how-can-i-make-ffmpeg-be-quieter-less-verbose#:~:text=Here%20you%20have%20loglevels%20from%20the%20source%20code%20(FFmpeg%20version%200.10.2.git)
+        # log levels for ffmpeg listed here:
+        # https://ffmpeg.org/ffmpeg.html#:~:text=loglevel%20is%20a%20string%20or%20a%20number%20containing%20one%20of%20the%20following%20values%3A
         self.__logLevel = kwargs.pop("logLevel", "error")
 
     def convert(self):
