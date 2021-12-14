a32cf195b9b23b05a16896d98167ef3f3c64c4f6
jg <57115019+joeyagreco@users.noreply.github.com>
2021-12-14 17:25:20 -0600

    moved file to util folder

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from VideoConverter import VideoConverter
+from util.VideoConverter import VideoConverter
 
 if __name__ == "__main__":
     vc = VideoConverter("H:\\Desktop-HDD\\test\\old.mov", "H:\\Desktop-HDD\\test\\new.mp4")
